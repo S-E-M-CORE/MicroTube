208aec2143ccf89280b8940f6488c72f8aaa7b68
Zatraka <71972672+Zatraka@users.noreply.github.com>
2024-09-03 11:47:22 +0200

    Second Pull request  (#2)
    * base Struktur + 2 Frontend Versionen.
    
    * create a React package
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -
    
    * new Icon for the Assert Folder and writting some CSS.
    
    * more CSS and Lennys Icons

---
+++ Frontend/Version_02/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+//import Router from 'react-router-dom'
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+
+    </div>
+  )
+}
+
+export default App
